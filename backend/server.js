834a881a9872ab95dcce166173e0e437f5a4224c
Hex-Umbra <g.p.dacosta@hotmail.com>
2025-09-23 19:39:35 +0200

    Trying to fix the CORS origin for the deployment process

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,8 @@ const allowedOrigins = (
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (!origin || allowedOrigins.length === 0 || allowedOrigins.includes(origin)) return callback(null, true);
+      if(!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, true);
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
