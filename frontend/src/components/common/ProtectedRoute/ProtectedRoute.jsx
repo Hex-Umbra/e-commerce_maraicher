e945a3330322bf11138ba5b493d1b740453da418
Hex-Umbra <g.p.dacosta@hotmail.com>
2025-12-07 20:05:34 +0100

    chore(debug): remove frontend debug logs

--- frontend/src/components/common/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/common/ProtectedRoute/ProtectedRoute.jsx
@@ -4,27 +4,18 @@ import { useAuth } from '../../../context/AuthContext';
 const ProtectedRoute = ({ requiredRole }) => {
     const { user, isAuthenticated, loading } = useAuth(); // Destructure loading
 
-    console.log('ProtectedRoute: isAuthenticated =', isAuthenticated);
-    console.log('ProtectedRoute: user =', user);
-    console.log('ProtectedRoute: requiredRole =', requiredRole);
-    console.log('ProtectedRoute: loading =', loading); // Log loading state
-
     if (loading) {
-        console.log('ProtectedRoute: Loading authentication status...');
         return <div>Loading...</div>; // Render loading indicator while authentication status is being determined
     }
 
     if (!isAuthenticated) {
-        console.log('ProtectedRoute: Not authenticated, redirecting to /login');
         return <Navigate to="/login" replace />;
     }
 
     if (requiredRole && user.role !== requiredRole) {
-        console.log('ProtectedRoute: User role mismatch, redirecting to /');
         return <Navigate to="/" replace />;
     }
 
-    console.log('ProtectedRoute: Access granted');
     return <Outlet />;
 };
 
